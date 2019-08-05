de5a86ca2cdc04f38feefb3fbf2d85347e2bd4a7
andy.bin <andy.bin@wolaidai.com>
2019-08-05 11:04:57 +0800

    主键自增长功能

---
+++ GiftMoney/Models/Trade.swift
@@ -0,0 +1,14 @@
+//
+//  Trade.swift
+//  GiftMoney
+//
+//  Created by andy.bin on 2019/8/5.
+//  Copyright © 2019 binea. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Trade: Object {
+    
+}
