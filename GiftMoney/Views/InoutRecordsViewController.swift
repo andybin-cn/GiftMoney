485f6d589726f6bc0c0639a94a36a1ae3d92bdbb
andy.bin <andy.bin@wolaidai.com>
2019-08-07 17:50:45 +0800

    首页列表界面

--- GiftMoney/Views/InoutRecordsViewController.swift
+++ GiftMoney/Views/InoutRecordsViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 import Common
 
-class InoutRecordsViewController: BaseViewController {
+class InoutRecordsViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView = UITableView()
+    var trades = [Trade]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +20,18 @@ class InoutRecordsViewController: BaseViewController {
         
         let addRecordButton = UIBarButtonItem(title: "新增", style: UIBarButtonItem.Style.plain, target: self, action: #selector(addRecordButtonTapped))
         self.navigationItem.rightBarButtonItems = [addRecordButton]
-
+        
+        
+        tableView.apply { (tableView) in
+            tableView.register(TradeCell.self, forCellReuseIdentifier: TradeCell.commonIdentifier)
+            tableView.estimatedRowHeight = 80
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+            tableView.addTo(self.view) { (make) in
+                make.edges.equalToSuperview()
+            }
+        }
     }
     
     override func addNavigationBar() {
@@ -37,4 +51,29 @@ class InoutRecordsViewController: BaseViewController {
         
         navigationController?.pushViewController(AddTradeViewController(), animated: true)
     }
+    
+    func loadData() {
+        trades = RealmManager.share.realm.objects(Trade.self).filter { _ in true }
+        tableView.reloadData()
+    }
+    
+    //MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trades.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TradeCell.commonIdentifier, for: indexPath) as! TradeCell
+        cell.selectionStyle = .none
+        let trade = trades[indexPath.row]
+        cell.configerUI(trade: trade)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let trade = trades[indexPath.row]
+        let controller = AddTradeViewController(trade: trade)
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
 }
