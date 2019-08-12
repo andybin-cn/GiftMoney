8c00769800772185af9f3ecdfc34db1ba4b02a09
andy.bin <andy.bin@wolaidai.com>
2019-08-12 16:00:07 +0800

    增加Manager

--- GiftMoney/Services/TradeManger.swift
+++ GiftMoney/Services/TradeManger.swift
@@ -11,14 +11,33 @@ import Foundation
 struct TradeEventGroupKey: Hashable {
     var name: String
     var time: Date
+    var lastUseTime: Date
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(time.toString(withFormat: "yyyy-MM-dd"))
+    }
 }
 
 
+
+
 class TradeManger {
-    var tradeGroups = Dictionary<TradeEventGroupKey, [Trade]>()
+    static let shared = TradeManger()
+    var inTradeGroups = Dictionary<TradeEventGroupKey, [Trade]>()
+    var outTradeGroups = Dictionary<TradeEventGroupKey, [Trade]>()
+    var outTrades = [Trade]()
     
-    
-    init() {
-        
+    private init() {
+        let intTrades = RealmManager.share.realm.objects(Trade.self).filter { item in item.type == Trade.TradeType.inAccount }
+        intTrades.forEach { (trade) in
+            let groupKey = TradeEventGroupKey(name: trade.eventName, time: trade.eventTime, lastUseTime: trade.updateTime)
+            if var tradeGroup = inTradeGroups[groupKey] {
+                tradeGroup.append(trade)
+            } else {
+                inTradeGroups[groupKey] = [trade]
+            }
+        }
+        outTrades = RealmManager.share.realm.objects(Trade.self).filter { item in item.type == Trade.TradeType.outAccount }
     }
 }
