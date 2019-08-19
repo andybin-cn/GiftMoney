d207b2e769500e1b5931b931b7fd79c42ada23a4
Andy.Bin <810018715@qq.com>
2019-08-19 09:26:24 +0800

    删除无用代码

--- GiftMoney/Services/TradeManger.swift
+++ GiftMoney/Services/TradeManger.swift
@@ -11,37 +11,12 @@ import PhotosUI
 import Common
 import RxSwift
 
-//struct TradeEventGroupKey: Hashable {
-//    var name: String
-//    var time: Date
-//    var lastUseTime: Date
-//
-//    func hash(into hasher: inout Hasher) {
-//        hasher.combine(name)
-//        hasher.combine(time.toString(withFormat: "yyyy-MM-dd"))
-//    }
-//}
-
-
-
 
 class TradeManger {
     static let shared = TradeManger()
-//    var inTradeGroups = Dictionary<TradeEventGroupKey, [Trade]>()
-//    var outTradeGroups = Dictionary<TradeEventGroupKey, [Trade]>()
-//    var outTrades = [Trade]()
     
     private init() {
-//        let intTrades = RealmManager.share.realm.objects(Trade.self).filter { item in item.type == Trade.TradeType.inAccount }
-//        intTrades.forEach { (trade) in
-//            let groupKey = TradeEventGroupKey(name: trade.eventName, time: trade.eventTime, lastUseTime: trade.updateTime)
-//            if var tradeGroup = inTradeGroups[groupKey] {
-//                tradeGroup.append(trade)
-//            } else {
-//                inTradeGroups[groupKey] = [trade]
-//            }
-//        }
-//        outTrades = RealmManager.share.realm.objects(Trade.self).filter { item in item.type == Trade.TradeType.outAccount }
+        
     }
     
     func eventsGroup() -> Dictionary<Event, [Trade]> {
