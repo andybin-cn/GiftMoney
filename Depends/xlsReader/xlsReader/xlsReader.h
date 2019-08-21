3422fc677baf6097c24cf6b298f70df9b80ab9a4
jim-binea <810018715@qq.com>
2019-08-21 15:56:34 +0800

    测试libxls

---
+++ Depends/xlsReader/xlsReader/xlsReader.h
@@ -0,0 +1,19 @@
+//
+//  xlsReader.h
+//  xlsReader
+//
+//  Created by andy.bin on 2019/8/21.
+//  Copyright © 2019 Binea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for xlsReader.
+FOUNDATION_EXPORT double xlsReaderVersionNumber;
+
+//! Project version string for xlsReader.
+FOUNDATION_EXPORT const unsigned char xlsReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xlsReader/PublicHeader.h>
+
+
--- GiftMoney/GiftMoney-Bridging-Header.h
+++ GiftMoney/GiftMoney-Bridging-Header.h
@@ -2,4 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifdef __OBJC__
+    #import <Foundation/Foundation.h>
+    #import <UIKit/UIKit.h>
+#endif
+
 #import "xlsxwriter.h"
+
+#import <xlsReader/xlsReader.h>
