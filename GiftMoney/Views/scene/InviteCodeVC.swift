5dbb52ae3283c18284f36076b582eecbc0be7f0e
jim-binea <810018715@qq.com>
2019-09-01 20:00:37 +0800

    UI调整，文案调整

--- GiftMoney/Views/scene/InviteCodeVC.swift
+++ GiftMoney/Views/scene/InviteCodeVC.swift
@@ -49,20 +49,43 @@ class InviteCodeVC: BaseViewController {
             }
         }
         
-        let label1 = UILabel(textColor: .appDarkText, font: .appFont(ofSize: 18), textAlignment: .left, text: "成功邀请 5 位好友，解锁【黄金VIP】所有功能")
-        label1.numberOfLines = 0
-        label1.lineBreakMode = .byWordWrapping
+        let item1 = MarketServiceItem(title: "成功邀请 5 位好友，解锁【黄金VIP】所有功能")
+        item1.title.textColor = .appDarkText
+        item1.icon.backgroundColor = .appMainRed
+        let item2 = MarketServiceItem(title: "成功邀请 20 位好友，解锁【钻石VIP】所有功能")
+        item2.title.textColor = .appDarkText
+        item2.icon.backgroundColor = .appMainRed
+//        let label1 = UILabel(textColor: .appDarkText, font: .appFont(ofSize: 18), textAlignment: .left, text: "成功邀请 5 位好友，解锁【黄金VIP】所有功能")
+//        label1.numberOfLines = 0
+//        label1.lineBreakMode = .byWordWrapping
         
-        let label2 = UILabel(textColor: .appDarkText, font: .appFont(ofSize: 18), textAlignment: .left, text: "成功邀请 30 位好友，解锁【钻石VIP】所有功能")
-        label2.numberOfLines = 0
-        label2.lineBreakMode = .byWordWrapping
+//        let label2 = UILabel(textColor: .appDarkText, font: .appFont(ofSize: 18), textAlignment: .left, text: "成功邀请 30 位好友，解锁【钻石VIP】所有功能")
+//        label2.numberOfLines = 0
+//        label2.lineBreakMode = .byWordWrapping
         
-        let label3 = UILabel(textColor: .appGrayText, font: .appFont(ofSize: 13), textAlignment: .left, text: "我的邀请码【\(inviteCode)】,告诉好友下载App后，填写你的邀请码就可以了。")
+        let attrubute = NSMutableAttributedString()
+        attrubute.append(NSAttributedString(string: "我的邀请码", attributes: [NSAttributedString.Key.font : UIFont.appFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.appGrayText]))
+        
+        attrubute.append(NSAttributedString(string: "【\(inviteCode)】", attributes: [NSAttributedString.Key.font : UIFont.appFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor.appMainRed]))
+        
+        attrubute.append(NSAttributedString(string: "\n告诉好友下载App后，填写你的邀请码就可以了。", attributes: [NSAttributedString.Key.font : UIFont.appFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.appGrayText]))
+        
+        
+        let label3 = UILabel()
+        label3.attributedText = attrubute
         label3.numberOfLines = 0
         label3.lineBreakMode = .byWordWrapping
         
         
-        let label4 = UILabel(textColor: .appDarkText, font: .appFont(ofSize: 18), textAlignment: .left, text: "您已成功邀请 \(inviteCount) 位好友!")
+        let attrubute2 = NSMutableAttributedString()
+        attrubute2.append(NSAttributedString(string: "您已成功邀请", attributes: [NSAttributedString.Key.font : UIFont.appFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.appGrayText]))
+        
+        attrubute2.append(NSAttributedString(string: " \(inviteCount) ", attributes: [NSAttributedString.Key.font : UIFont.appFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.appMainRed]))
+        
+        attrubute2.append(NSAttributedString(string: "位好友!", attributes: [NSAttributedString.Key.font : UIFont.appFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.appGrayText]))
+        
+        let label4 = UILabel()
+        label4.attributedText = attrubute2
         label4.numberOfLines = 0
         label4.lineBreakMode = .byWordWrapping
         
@@ -95,15 +118,15 @@ class InviteCodeVC: BaseViewController {
         buttons.addArrangedSubview(button1)
         buttons.addArrangedSubview(button2)
         
-        stackView.addArrangedSubview(label1)
-        stackView.addArrangedSubview(label2)
+        stackView.addArrangedSubview(item1)
+        stackView.addArrangedSubview(item2)
         stackView.addArrangedSubview(label3)
         stackView.addArrangedSubview(label4)
         stackView.addArrangedSubview(buttons)
     }
     
     @objc func inviteButtonTapped() {
-        let controller = UIActivityViewController(activityItems: ["给你推荐一款实用App，记得填写我的邀请码：\(inviteCode)", URL(string: "https://apps.apple.com/cn/app/id1478354248")!], applicationActivities: nil)
+        let controller = UIActivityViewController(activityItems: ["推荐一款App，我的邀请码：\(inviteCode)", URL(string: "https://apps.apple.com/cn/app/id1478354248")!], applicationActivities: nil)
         self.present(controller, animated: true, completion: nil)
     }
     
