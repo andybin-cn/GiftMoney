cc82fadc3231ae19169845abbc49a6a5d4517420
jim-binea <810018715@qq.com>
2019-09-11 10:40:11 +0800

    语音识别

---
+++ GiftMoney/Views/scene/SpeechViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SpeechViewController.swift
+//  GiftMoney
+//
+//  Created by andy.bin on 2019/9/10.
+//  Copyright © 2019 binea. All rights reserved.
+//
+
+import Foundation
+import Common
+
+class SpeechViewController: BaseViewController, UIViewControllerTransitioningDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func startRecognizer() {
+        
+    }
+}
